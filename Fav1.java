8fb9cb048ea26f75592447da2accb6c4c2eff4c4
sumi174 <sumiakter1552@gmail.com>
2025-10-16 23:13:19 +0600

    Add files via upload

---
+++ Fav1.java
@@ -0,0 +1,5 @@
+public class Fav1 {
+    public static void main(String[] args) {
+        IceCream i = new IceCream("Vanilla"); 
+        System.out.println("Favourite ice cream: " + i.getFlavour());     }
+}
---
+++ IceCream1.java
@@ -0,0 +1,12 @@
+class IceCream {
+    private String flavour;
+    
+    public IceCream(String flavour) {
+        this.flavour = flavour;
+    }
+
+    public String getFlavour() {
+        return flavour;
+    }
+}
+
