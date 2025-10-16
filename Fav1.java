78638672099419403e01cacd8450d9ef8ce6d7ff
sumi174 <sumiakter1552@gmail.com>
2025-10-16 23:13:43 +0600

    Delete Fav1.java

--- Fav1.java
+++
@@ -1,5 +0,0 @@
-public class Fav1 {
-    public static void main(String[] args) {
-        IceCream i = new IceCream("Vanilla"); 
-        System.out.println("Favourite ice cream: " + i.getFlavour());     }
-}
