d31a20dcb1a26878ff9d174b42de6955ccddfd87
sumi174 <sumiakter1552@gmail.com>
2025-10-16 23:09:04 +0600

    Add files via upload

---
+++ Assignment2/IceCream.java
@@ -0,0 +1,14 @@
+class IceCream {
+    private String flavour; // private variable
+
+    // setter method
+    public void setFlavour(String flavour) {
+        this.flavour = flavour;
+    }
+
+    // getter method
+    public String getFlavour() {
+        return flavour;
+    }
+}
+
