bb578ecdec2ef3d88b05069dc3677e691846f903
sumi174 <sumiakter1552@gmail.com>
2025-10-16 23:24:54 +0600

    Add files via upload

---
+++ Assignment2/Fav1.java
@@ -0,0 +1,6 @@
+public class Fav1 {
+    public static void main(String[] args) {
+        IceCream i = new IceCream("Mango");
+        System.out.println("Favourite ice cream: " + i.flavour);
+    }
+}
---
+++ Assignment2/Fav3.java
@@ -0,0 +1,6 @@
+public class Fav3{
+    public static void main(String[] args) {
+        IceCream i = new IceCream("Chocolate");
+        System.out.println(i);
+    }
+}
