4f774a2eea68eb35643476f6886f8cfdc6269304
sumi174 <sumiakter1552@gmail.com>
2025-10-16 22:50:09 +0600

    Add files via upload

---
+++ Assignment/Mobile.java
@@ -0,0 +1,26 @@
+package com;
+
+public class Mobile {
+    private String brand;
+    private int price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void MobileStatus() {
+        System.out.println("Mobile Status: Available in stock!");
+    }
+}
